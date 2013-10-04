d2b8143b31552a37667b3cce52bb9e32c0110a79
Sweekriti Satpathy <sweek@bu.edu>
2013-10-03 20:42:26 -0400

    Changes to UI for Prototype
    Just the launch icon is changed and the main page is setup. working on
    the other pages

--- EDUkid/gen/bu/edu/cs673/edukid/R.java
+++ EDUkid/gen/bu/edu/cs673/edukid/R.java
@@ -27,28 +27,37 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int apple=0x7f020000;
-        public static final int edukid=0x7f020001;
-        public static final int edukidicon=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int abc1=0x7f020000;
+        public static final int abctile=0x7f020001;
+        public static final int apple=0x7f020002;
+        public static final int basicback=0x7f020003;
+        public static final int bottombanner=0x7f020004;
+        public static final int colors=0x7f020005;
+        public static final int edukid=0x7f020006;
+        public static final int edukidicon=0x7f020007;
+        public static final int gamepage=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int numb1=0x7f02000a;
+        public static final int shapes=0x7f02000b;
     }
     public static final class id {
-        public static final int accountDetails=0x7f08000f;
-        public static final int backButton=0x7f08000e;
-        public static final int edukidImage=0x7f080001;
-        public static final int exitButton=0x7f080002;
+        public static final int accountDetails=0x7f080010;
+        public static final int backButton=0x7f08000f;
+        public static final int exitButton=0x7f080001;
+        public static final int imageView1=0x7f080008;
         public static final int learnAlphabetButton=0x7f080004;
         public static final int learnColorsButton=0x7f080007;
-        public static final int learnContentGrid=0x7f08000d;
-        public static final int learnContentGridButton=0x7f08000c;
-        public static final int learnContentImage=0x7f080009;
-        public static final int learnContentItem=0x7f08000b;
-        public static final int learnContentViewPager=0x7f080008;
-        public static final int learnContentWord=0x7f08000a;
+        public static final int learnContentGrid=0x7f08000e;
+        public static final int learnContentGridButton=0x7f08000d;
+        public static final int learnContentImage=0x7f08000a;
+        public static final int learnContentItem=0x7f08000c;
+        public static final int learnContentViewPager=0x7f080009;
+        public static final int learnContentWord=0x7f08000b;
         public static final int learnNumbersButton=0x7f080005;
         public static final int learnShapesButton=0x7f080006;
-        public static final int listView1=0x7f080010;
-        public static final int settingsButton=0x7f080003;
+        public static final int linearLayout1=0x7f080003;
+        public static final int listView1=0x7f080011;
+        public static final int settingsButton=0x7f080002;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
