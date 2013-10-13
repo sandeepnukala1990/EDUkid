b646885adb8a2ff14e3ec4d63341dd7560314933
Jasjot <jasjot_jrarjn1@yahoo.in>
2013-10-13 00:13:33 -0400

    adding splash screen

---
+++ EDUkid/src/bu/edu/cs673/edukid/EDUsplash.java
@@ -0,0 +1,35 @@
+package bu.edu.cs673.edukid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class EDUsplash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edusplash);
+		
+		Thread Timer = new Thread()
+		{
+			public void run(){
+				try{
+					sleep(4000);
+					Intent intent = new Intent(EDUsplash.this, EDUkid.class);
+					startActivity(intent);
+				}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			finally{
+				finish();
+			}
+		
+			}
+		
+		};
+		Timer.start();
+	}
+}
