f6b214533606d62bd3c2f189dbbe7006e7c40feb
Jasjot <jasjot_jrarjn1@yahoo.in>
2013-10-21 01:33:19 -0400

    for numbers, colours,shapes functionality (reverted from commit b4a2824d76a272874cbaf12c72e26b95e265302e)

--- EDUkid/src/bu/edu/cs673/edukid/learn/LearnContentView.java
+++ EDUkid/src/bu/edu/cs673/edukid/learn/LearnContentView.java
@@ -27,7 +27,7 @@ public class LearnContentView extends FragmentActivity {
 		learnType = (LearnType) extras.get("LearnType");
 		int position = extras.getInt("Position");
 		
-		// SetupBundle extras = intent.getExtras() view pager
+		// Setup view pager
 		learnContentViewPagerAdapter = new LearnContentViewPagerAdapter(
 				getSupportFragmentManager(), learnType);
 
