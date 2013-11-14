c6653585eba92968552e2c591b846b518e3af16c
raks91 <raks91@bu.edu>
2013-11-13 20:11:48 -0600

    Timer added . alert after 3 minutes for testing!

--- EDUkid/src/bu/edu/cs673/edukid/EDUkid.java
+++ EDUkid/src/bu/edu/cs673/edukid/EDUkid.java
@@ -7,6 +7,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
@@ -41,6 +43,18 @@ public class EDUkid extends Activity implements OnClickListener {
 
 	public static final String WORD_INDEX = "WordIndex";
 
+	private TextView timerValue;
+
+	private long startTime = 0L;
+
+	private Handler customHandler = new Handler();
+
+	long timeInMilliseconds = 0L;
+
+	long timeSwapBuff = 0L;
+
+	long updatedTime = 0L;
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -48,10 +62,60 @@ public class EDUkid extends Activity implements OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edukid);
+		final Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			public void run() {
+				AlertDialog.Builder alert = new AlertDialog.Builder(EDUkid.this);
+				alert.setTitle("Timed OUT!");
+				alert.setPositiveButton("Ok",
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// TODO
+
+							}
+						});
+				alert.create();
+				alert.show();
+			}
+		}, 180000);
+		timerValue = (TextView) findViewById(R.id.timerValue);
+
+		startTime = SystemClock.uptimeMillis();
+		updateTimerThread.run();
 
 		setupCategoryButtons();
 		welcomeUserBack(true);
 	}
+
+	private Runnable updateTimerThread = new Runnable() {
+
+		public void run() {
+
+			timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+			updatedTime = timeSwapBuff + timeInMilliseconds;
+
+			int secs = (int) (updatedTime / 1000);
+
+			int mins = secs / 60;
+
+			secs = secs % 60;
+
+			int milliseconds = (int) (updatedTime % 1000);
+
+			timerValue.setText("" + mins + ":"
+
+			+ String.format("%02d", secs) + ":"
+
+			+ String.format("%03d", milliseconds));
+
+			customHandler.postDelayed(this, 0);
+
+		}
+
+	};
 
 	/**
 	 * {@inheritDoc}
@@ -136,6 +200,7 @@ public class EDUkid extends Activity implements OnClickListener {
 	 *            the view used in the callback.
 	 */
 	public void onSettingsClick(View view) {
+
 		final MathProblem mathProblem = MathProblemGenerator
 				.generateMathProblem();
 
