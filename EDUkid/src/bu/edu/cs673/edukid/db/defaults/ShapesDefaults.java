ec5f4312e7e92cfcbaf1961276b6ce11e1703789
isharana <isharana@bu.edu>
2013-11-23 14:45:32 -0500

    changes to the shapes defaults

--- EDUkid/src/bu/edu/cs673/edukid/db/defaults/ShapesDefaults.java
+++ EDUkid/src/bu/edu/cs673/edukid/db/defaults/ShapesDefaults.java
@@ -6,42 +6,29 @@ import bu.edu.cs673.edukid.db.model.Word;
 public class ShapesDefaults {
 
 	protected static String[] DEFAULT_SHAPES = new String[] { "Circle",
-			"Square", "Diamond", "Oval", "Triangle", "Pentagon", "Hexagon",
-			"Star" };
+			"Square","Triangle", "Rectangle",
+		 };
 
 	// TODO: replace the edukid icon with the real image
 	private static Word[] CIRCLE_WORDS = new Word[] { new Word("Circle",
-			R.drawable.edukidicon) };
+			R.drawable.circlepng) };
 
 	// TODO: replace the edukid icon with the real image
 	private static Word[] SQUARE_WORDS = new Word[] { new Word("Square",
-			R.drawable.edukidicon) };
-
-	// TODO: replace the edukid icon with the real image
-	private static Word[] DIAMOND_WORDS = new Word[] { new Word("Diamond",
-			R.drawable.edukidicon) };
-
-	// TODO: replace the edukid icon with the real image
-	private static Word[] OVAL_WORDS = new Word[] { new Word("Oval",
-			R.drawable.edukidicon) };
+			R.drawable.squarepng) };
 
 	// TODO: replace the edukid icon with the real image
 	private static Word[] TRIANGLE_WORDS = new Word[] { new Word("Triangle",
-			R.drawable.edukidicon) };
+			R.drawable.trianglepng) };
 
 	// TODO: replace the edukid icon with the real image
-	private static Word[] PENTAGON_WORDS = new Word[] { new Word("Pentagon",
-			R.drawable.edukidicon) };
+	private static Word[] RECTANGLE_WORDS = new Word[] { new Word("Rectangle",
+			R.drawable.rectanglepng) };
 
-	// TODO: replace the edukid icon with the real image
-	private static Word[] HEXAGON_WORDS = new Word[] { new Word("Hexagon",
-			R.drawable.edukidicon) };
-
-	// TODO: replace the edukid icon with the real image
-	private static Word[] STAR_WORDS = new Word[] { new Word("Star",
-			R.drawable.edukidicon) };
+	
+	
 
 	protected static Word[][] SHAPES_WORDS = new Word[][] { CIRCLE_WORDS,
-			SQUARE_WORDS, DIAMOND_WORDS, OVAL_WORDS, TRIANGLE_WORDS,
-			PENTAGON_WORDS, HEXAGON_WORDS, STAR_WORDS };
+			SQUARE_WORDS, TRIANGLE_WORDS,
+			RECTANGLE_WORDS,  };
 }
