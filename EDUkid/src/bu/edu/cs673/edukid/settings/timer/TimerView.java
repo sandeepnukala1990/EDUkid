43f431df6e890f49bcff02cd0b7f2c87e541b69c
sandyimu <sandeepcrossfade@gmail.com>
2013-11-27 11:31:18 -0800

    Timer page has enable disable..

--- EDUkid/src/bu/edu/cs673/edukid/settings/timer/TimerView.java
+++ EDUkid/src/bu/edu/cs673/edukid/settings/timer/TimerView.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ToggleButton;
 import bu.edu.cs673.edukid.R;
 
 public class TimerView extends Activity {
 
-	private String time;
+	
 	
 
 	/**
@@ -18,9 +19,29 @@ public class TimerView extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.timer);
-		
-		Button btn=(Button) findViewById(R.id.createSaveButton);
+		final ToggleButton btn = (ToggleButton) findViewById(R.id.toggleButton);
+		final Button btn1 = (Button) findViewById(R.id.button1);
+		final EditText time = (EditText) findViewById(R.id.playTime);
+		time.setEnabled(false);
+		btn1.setEnabled(false);
 		btn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				if (btn.isChecked()) {
+					time.setEnabled(true);
+					btn1.setEnabled(true);
+				} else {
+					time.setEnabled(false);
+					btn1.setEnabled(false);
+				}
+			}
+		});
+
+	
+		Button btn2=(Button) findViewById(R.id.createSaveButton);
+		btn2.setOnClickListener(new View.OnClickListener() {
 			
 		
 
@@ -32,8 +53,8 @@ public class TimerView extends Activity {
 				
 			
 		});
-		EditText time = (EditText)findViewById(R.id.playTime);
-		time.setOnClickListener(new View.OnClickListener() {
+		EditText time1 = (EditText)findViewById(R.id.playTime);
+		time1.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
