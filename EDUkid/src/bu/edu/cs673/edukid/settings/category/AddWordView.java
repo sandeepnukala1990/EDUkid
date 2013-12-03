e076b233b5a18044d20e0bc6435b397586fa0488
Jasjot <jasjot_jrarjn1@yahoo.in>
2013-12-03 14:07:39 -0500

    add word view, but still working on image resizing and images

--- EDUkid/src/bu/edu/cs673/edukid/settings/category/AddWordView.java
+++ EDUkid/src/bu/edu/cs673/edukid/settings/category/AddWordView.java
@@ -1,21 +1,51 @@
 package bu.edu.cs673.edukid.settings.category;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import bu.edu.cs673.edukid.EDUkid;
 import bu.edu.cs673.edukid.R;
+import bu.edu.cs673.edukid.db.Database;
 import bu.edu.cs673.edukid.db.ImageUtils;
 import bu.edu.cs673.edukid.db.model.Word;
 import bu.edu.cs673.edukid.db.model.category.CategoryType;
 
-public class AddWordView extends Activity {
+public class AddWordView extends Activity implements OnClickListener {
 
 	private CategoryType categoryType;
 
 	private int itemIndex;
 
+	private static final int TAKE_PICTURE = 1888;
+
+	private boolean mStartReco = true;
+
+	private static final long DATABASE_ERROR = -1;
+
+	private String word;
+
+	private ImageView wordImage;
+
+	private ImageView micImage;
+
+	private Database database = Database.getInstance(this);
+
+	public MediaRecorder recorder = new MediaRecorder();
+	private String mFileName = "";
+	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -23,6 +53,8 @@ public class AddWordView extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_word);
+		wordImage = (ImageView) findViewById(R.id.imageView1);
+		micImage = (ImageView) findViewById(R.id.imageButton2);
 
 		Intent intent = getIntent();
 		Bundle extras = intent.getExtras();
@@ -30,11 +62,18 @@ public class AddWordView extends Activity {
 		categoryType = (CategoryType) extras
 				.getSerializable(EDUkid.CATEGORY_TYPE);
 		itemIndex = extras.getInt(EDUkid.ITEM_INDEX);
+			
+		Button createSaveButton = (Button) findViewById(R.id.button1);
+		createSaveButton.setOnClickListener(this);
+		ImageButton createUploadPhotoButton = (ImageButton) findViewById(R.id.imageButton1);
+		createUploadPhotoButton.setOnClickListener(this);
+		micImage.setOnClickListener(this);
+		//TextView balh = (TextView) findViewById(R.id.textView233);
 	}
 
 	// TODO: just temporary for now. Need to get input from the user and use
 	// that instead of hardcoding.
-	public void onAddWordClick(View view) {
+	/*public void onAddWordClick(View view) {
 		// TODO: do this based on user entry
 		// TODO: hard coding these for now to add a new word.
 		Word word = new Word();
@@ -46,5 +85,95 @@ public class AddWordView extends Activity {
 		categoryType.addItemWord(itemIndex, word);
 
 		// TODO: refresh list
+	}*/
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == TAKE_PICTURE && resultCode == RESULT_OK) {
+			Bitmap photo = (Bitmap) data.getExtras().get("data");
+
+			if (photo != null) {
+				wordImage.setImageBitmap(photo);
+			}
+		}
+	}
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId())
+		{
+		case R.id.button1:
+			word=((EditText) findViewById(R.id.editText1))
+					.getText().toString();
+			//long result = DATABASE_ERROR;
+			Word w=new Word();
+			w.setWord(word);
+			if(mFileName.equalsIgnoreCase(""))
+				w.setWordSound(word);
+			else
+				w.setWordSound(mFileName);
+			
+			w.setWordImage(ImageUtils.drawableToByteArray(wordImage
+					.getDrawable()));
+			categoryType.addItemWord(itemIndex, w);
+			
+			Toast.makeText(this, "Word saved successfully!",
+					Toast.LENGTH_LONG).show();
+			break;
+		
+		case R.id.imageButton1:
+			startCamera();
+			break;
+			
+		case R.id.imageButton2:
+			onRecord(mStartReco);
+			mStartReco = !mStartReco;
+			break;
+		}
+	}
+
+	private void onRecord(boolean start) {
+		// TODO Auto-generated method stub
+		if (start) {
+			startRecording();
+
+		} else {
+			stopRecording();
+		}
+		
+	}
+
+	private void stopRecording() {
+		// TODO Auto-generated method stub
+		micImage.setBackgroundResource(R.drawable.mikebutton);
+		recorder.stop();
+		recorder.release();
+		recorder = null;
+	}
+
+	private void startRecording() {
+		// TODO Auto-generated method stub
+		micImage.setBackgroundResource(R.drawable.recordmikebutton);
+		recorder = new MediaRecorder();
+		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+		mFileName += "/audiorecordtest.3gp";
+		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+		recorder.setOutputFile(mFileName);
+		try {
+			recorder.prepare();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		recorder.start();
+	}
+
+	private void startCamera() {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+		startActivityForResult(intent, TAKE_PICTURE);
 	}
 }
