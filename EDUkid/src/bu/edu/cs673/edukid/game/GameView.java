6f6fc50c82a517e9fb9a52db426da677822efa06
Sweekriti Satpathy <sweek@bu.edu>
2013-12-03 23:42:30 -0500

    Game Mode DONE!!
    Just making a custom toast for it now

--- EDUkid/src/bu/edu/cs673/edukid/game/GameView.java
+++ EDUkid/src/bu/edu/cs673/edukid/game/GameView.java
@@ -19,7 +19,12 @@ import bu.edu.cs673.edukid.db.model.category.CategoryType;
 
 public class GameView extends Activity{
 	private CategoryType categoryType; 
-	
+	private TextView correctScore;
+	private TextView wrongScore;
+	private int correct=0;
+	private int wrong=0;
+	int MainID; //to store Letter for the Question
+	int[] letterTracker; //Array containing the letters for each button
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,6 +36,18 @@ public class GameView extends Activity{
 		categoryType = (CategoryType) extras
 				.getSerializable(EDUkid.CATEGORY_TYPE);
 		
+		Database.getInstance(this);
+		correctScore = (TextView) findViewById(R.id.correctScore);
+		wrongScore = (TextView) findViewById(R.id.wrongScore);
+		
+		setQuestions();
+		//this set the letter to C for MainID=2
+		
+		
+	}
+	
+	public void setQuestions(){
+
 		Random rand = new Random();		
 		
 		TextView alphabet = (TextView) findViewById(R.id.gamemodeAlphabet);
@@ -39,20 +56,18 @@ public class GameView extends Activity{
 		ImageButton image3= (ImageButton) findViewById(R.id.gameImage3);
 		ImageButton image4= (ImageButton) findViewById(R.id.gameImage4);
 		
-		
-		Database.getInstance(this);
-		
-		
-		//sets image to that of mainID of letter
-		//int image1ID= categoryType.getLearnItemDrawableId(MainID, 0);		
-		//image1.setImageResource(image1ID);
-		
-		int[] letterTracker = new int[4];
+	
+		letterTracker = new int[4];
 		int imageID;
 		for(int i=0; i<4; i++){
 			
 			int option=rand.nextInt(26);
+			//check for repeated letter after 1st letter is set
+			if((letterTracker[i]==option)){
+				option=rand.nextInt(26);
+			}
 			letterTracker[i]= option;
+			
 			
 			switch(i){
 			
@@ -77,17 +92,73 @@ public class GameView extends Activity{
 		}
 		
 
-		int MainID=letterTracker[rand.nextInt(3)];
+		MainID=letterTracker[rand.nextInt(3)];
 		String letter = DatabaseDefaults.getAlphabet()[MainID];
 		alphabet.setText(letter);
-		//this set the letter to C for MainID=2
-		
-		
 	}
 	
-	public void onImageClick(View view) {
-		Toast.makeText(this, "Add item coming soon...", Toast.LENGTH_SHORT)
+	public void onImageClick1(View view) {
+		if(MainID==letterTracker[0]){	
+			correct++;
+			correctScore.setText(""+correct);
+		Toast.makeText(this, "Correct Answer! Well Done! ", Toast.LENGTH_SHORT)
 				.show();
+		}
+		else{
+			wrong++;
+			wrongScore.setText(""+wrong);
+			Toast.makeText(this, "Wrong Answer!Try Again :) ", Toast.LENGTH_SHORT)
+			.show();
+		}
+	}
+	
+	public void onImageClick2(View view) {		
+		if(MainID==letterTracker[1]){	
+			correct++;
+			correctScore.setText(""+correct);
+			Toast.makeText(this, "Correct Answer!", Toast.LENGTH_SHORT)
+					.show();
+			}
+		else{
+			wrong++;
+			wrongScore.setText(""+wrong);
+			Toast.makeText(this, "Wrong Answer!Try Again :) ", Toast.LENGTH_SHORT)
+			.show();
+		}
+	}
+	
+	public void onImageClick3(View view) {
+		if(MainID==letterTracker[2]){
+			correct++;
+			correctScore.setText(""+correct);
+			Toast.makeText(this, "Correct Answer!", Toast.LENGTH_SHORT)
+					.show();
+			}
+		else{
+			wrong++;
+			wrongScore.setText(""+wrong);
+			Toast.makeText(this, "Wrong Answer!Try Again :) ", Toast.LENGTH_SHORT)
+			.show();
+		}
+	}
+	
+	public void onImageClick4(View view) {
+		if(MainID==letterTracker[3]){	
+			correct++;
+			correctScore.setText(""+correct);
+			Toast.makeText(this, "Correct Answer!", Toast.LENGTH_SHORT)
+					.show();
+			}
+		else{
+			wrong++;
+			wrongScore.setText(""+wrong);
+			Toast.makeText(this, "Wrong Answer!Try Again :) ", Toast.LENGTH_SHORT)
+			.show();
+		}
+	}
+	
+	public void onRefreshClick(View view){
+		setQuestions();
 	}
 	
 	
